7f2e7d2da554e757c9edb8d24d866dbcf24b9c1a
Matt Radbourne <m@radbourne.me>
2016-12-04 20:09:01 +0000

    Added Flic button compatibility

--- whiteboard-nanny.js
+++ whiteboard-nanny.js
@@ -5,6 +5,12 @@ var config = require('./config');
 var RaspiCam = require("raspicam");
 var RpiLeds = require('rpi-leds');
 var leds = new RpiLeds();
+var fliclib = require("./lib/fliclibNodeJs");
+var FlicClient = fliclib.FlicClient;
+var FlicConnectionChannel = fliclib.FlicConnectionChannel;
+var FlicScanner = fliclib.FlicScanner;
+
+var flic = new FlicClient("localhost", 5551);
 
 var camera = new RaspiCam({
 	mode: 'photo',
@@ -16,27 +22,26 @@ var camera = new RaspiCam({
 
 slack = new Slack(config.slackApiToken);
 
+
+
+
+
+flic.once("ready", function() {
+	console.log("Connected to daemon!");
+	flic.getInfo(function(info) {
+		info.bdAddrOfVerifiedButtons.forEach(function(bdAddr) {
+			listenToButton(bdAddr);
+		});
+	});
+});
+
 leds.status.blink();
 
 term.grabInput();
 
 term.on( 'key' , function( name , matches , data ) {
   if ( name === 'ENTER' ) {
-	  leds.status.turnOff();
-	  camera.start();
-	  camera.on('read', function(err, timestamp, filename) {
-		  if (filename === 'capture.jpg') {
-		slack.api('files.upload', {
-			file: fs.createReadStream('./capture.jpg'),
-			filename: config.whiteboardName + '_' + timestamp + '.jpg',
-			title: config.whiteboardName + ' ' + timestamp,
-			initial_comment: config.whiteboardName + ' has just been cleared',
-			channels: '#general'
-		}, function(err, response){
-			leds.status.blink();
-		});
-		}
-	});
+		postToSlack();
   } else if ( name === 'CTRL_C' ) {
     term.grabInput( false ) ;
     leds.status.reset();
@@ -46,3 +51,57 @@ term.on( 'key' , function( name , matches , data ) {
 
 term.colorRgb(250,77,25).bold( 'Welcome to TAB whiteboard nanny\n' ) ;
 term.defaultColor( 'Hit CTRL-C to quit.\n\n' ) ;
+
+
+
+
+
+
+function listenToButton(bdAddr) {
+	var cc = new FlicConnectionChannel(bdAddr);
+	flic.addConnectionChannel(cc);
+	cc.on("buttonUpOrDown", function(clickType, wasQueued, timeDiff) {
+		console.log(bdAddr + " " + clickType + " " + (wasQueued ? "wasQueued" : "notQueued") + " " + timeDiff + " seconds ago");
+		if (clickType === 'ButtonUp' && !wasQueued) {
+			postToSlack();
+		}
+	});
+	cc.on("connectionStatusChanged", function(connectionStatus, disconnectReason) {
+		console.log(bdAddr + " " + connectionStatus + (connectionStatus == "Disconnected" ? " " + disconnectReason : ""));
+	});
+}
+
+flic.on("bluetoothControllerStateChange", function(state) {
+	console.log("Bluetooth controller state change: " + state);
+});
+
+flic.on("newVerifiedButton", function(bdAddr) {
+	console.log("A new button was added: " + bdAddr);
+	listenToButton(bdAddr);
+});
+
+flic.on("error", function(error) {
+	console.log("Daemon connection error: " + error);
+});
+
+flic.on("close", function(hadError) {
+	console.log("Connection to daemon is now closed");
+});
+
+function postToSlack() {
+	leds.status.turnOff();
+	camera.start();
+	camera.on('read', function(err, timestamp, filename) {
+		if (filename === 'capture.jpg') {
+			slack.api('files.upload', {
+				file: fs.createReadStream('./capture.jpg'),
+				filename: config.whiteboardName + '_' + timestamp + '.jpg',
+				title: config.whiteboardName + ' ' + timestamp,
+				initial_comment: config.whiteboardName + ' has just been cleared',
+				channels: '#general'
+			}, function(err, response){
+				leds.status.blink();
+			});
+		}
+	});
+}
