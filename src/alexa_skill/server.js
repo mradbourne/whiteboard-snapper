ed29851d4a32fbf85a2894ea20787e1fa46946b0
Matt Radbourne <m@radbourne.me>
2017-10-13 22:57:43 +0100

    Move capture tool to src/ folder

---
+++ src/alexa_skill/server.js
@@ -0,0 +1,1 @@
+console.log('Alexa skill goes here')
--- src/capture_tool/server.js
+++ src/capture_tool/server.js
@@ -1,12 +1,12 @@
 const RaspiCam = require('raspicam')
 const Slack = require('slack-node')
 const RpiLeds = require('rpi-leds')
-const config = require('./config')
+const config = require('../../config')
 const startCli = require('./cli')
 
 const camera = new RaspiCam({
   mode: 'photo',
-  output: './captures/capture.jpg',
+  output: '../../captures/capture.jpg',
   width: 1620,
   height: 1232,
   quality: 80
