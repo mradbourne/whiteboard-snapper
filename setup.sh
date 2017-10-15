7fa5aa01aa38092f5e4fe4af00b852b27e96851d
Matt Radbourne <m@radbourne.net>
2017-10-15 00:18:34 +0000

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 if type node > /dev/null; then
   npm install
   read -p 'Enter the Slack API token for the team you want to use: ' slack_api_token
+  read -p 'Enter the default Slack channel to post to if none is specified: #' slack_default_channel
   read -p 'Enter a name for the whiteboard (e.g. "board room whiteboard"): ' whiteboard_name
 
   echo "const config = {
   slackApiToken: '$slack_api_token',
+  slackApiToken: '$slack_default_channel',
   whiteboardName: '$whiteboard_name'
 }
 
